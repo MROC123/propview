1786d174037af7debf9285a6e76648b22755f12b
OliverPawelski <pawelskiandoliver2003@gmail.com>
2024-05-13 16:04:15 +0100

    added models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,49 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_114835) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_13_145802) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "managers", force: :cascade do |t|
+    t.text "address"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_managers_on_user_id"
+  end
+
+  create_table "properties", force: :cascade do |t|
+    t.string "name"
+    t.text "address"
+    t.string "type"
+    t.string "bedrooms"
+    t.string "bathrooms"
+    t.bigint "manager_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["manager_id"], name: "index_properties_on_manager_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.bigint "property_id"
+    t.bigint "manager_id", null: false
+    t.text "pros"
+    t.text "cons"
+    t.text "advice"
+    t.float "rating"
+    t.date "start_date"
+    t.date "end_date"
+    t.float "service_charge"
+    t.float "rent"
+    t.text "location"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["manager_id"], name: "index_reviews_on_manager_id"
+    t.index ["property_id"], name: "index_reviews_on_property_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +66,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_114835) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "managers", "users"
+  add_foreign_key "properties", "managers"
+  add_foreign_key "reviews", "managers"
+  add_foreign_key "reviews", "properties"
+  add_foreign_key "reviews", "users"
 end
